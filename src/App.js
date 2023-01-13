03d7e98d34eebde16035ed8f6ce59da877485668
sahil jaryal <sahil199926@gmail.com>
2023-01-13 19:55:37 +0530

    darkmode

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import logo from './logo.svg';
 import Navs from './Navs';
 import './App.css';
 import {UserProvider} from './context/userContext'
+import {DarkProvider} from './context/DarkmodeContext'
 function App() {
   return (
     <div className="App">
       <UserProvider>
+        <DarkProvider>
       <Navs/>
+      </DarkProvider>
       </UserProvider>
     </div>
   );
--- src/components/Chat/ResentChat/index.js
+++ src/components/Chat/ResentChat/index.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { darkContext } from '../../../context/DarkmodeContext'
 
 function ResentChat() {
+  const [state, dispatch] = useContext(darkContext)
+  const toggleTheme = (color) => {
+    dispatch({
+      type: color
+    })
+  }
   return (
-    <div>ResentChat</div>
+    <div>
+      <button
+      onClick={()=>toggleTheme('MAKE_DARK')}
+      >toggle theme dark</button>
+       <button
+      onClick={()=>toggleTheme('MAKE_LIGHT')}
+      >toggle theme light</button>
+      <div
+      style={{
+        background: state.background,
+        color: state.font1,
+        width: '100px',
+        height: '100px'
+      }}
+      >test</div>
+    </div>
   )
 }
 
